c7e9f0517d3dcbed2c2e7e7ed19c5c9d07df13b3
Kartik Ohri <kartikohri13@gmail.com>
2023-10-03 17:59:22 +0530

    add email update for users

--- metabrainz/supporter/views.py
+++ metabrainz/supporter/views.py
@@ -181,13 +181,13 @@ def signup_noncommercial():
     return render_template("supporters/signup-non-commercial.html", form=form)
 
 
-@supporters_bp.route('/profile')
+@supporters_bp.route('/supporters/profile')
 @login_required
 def profile():
-    return render_template("supporters/profile.html")
+    return render_template("supporters/profile.html", current_supporter=current_user.supporter)
 
 
-@supporters_bp.route('/profile/edit', methods=['GET', 'POST'])
+@supporters_bp.route('/supporters/profile/edit', methods=['GET', 'POST'])
 @login_required
 def profile_edit():
     if current_user.is_commercial:
@@ -205,7 +205,7 @@ def profile_edit():
         if not current_user.is_commercial:
             kwargs["datasets"] = form.datasets.data
 
-        current_user.update(**kwargs)
+        current_user.supporter.update(**kwargs)
         flash.success("Profile updated.")
         return redirect(url_for('.profile'))
     else:
@@ -218,11 +218,11 @@ def profile_edit():
     return render_template('supporters/profile-edit.html', form=form)
 
 
-@supporters_bp.route('/profile/regenerate-token', methods=['POST'])
+@supporters_bp.route('/supporters/profile/regenerate-token', methods=['POST'])
 @login_required
 def regenerate_token():
     try:
-        return jsonify({'token': current_user.generate_token()})
+        return jsonify({'token': current_user.supporter.generate_token()})
     except InactiveSupporterException:
         raise BadRequest(gettext("Can't generate new token unless account is active."))
     except TokenGenerationLimitException as e:
